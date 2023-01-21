c38549818a40acbf22b6b7552e7591f814ecba29
justcryd <adm.nkvz@gmail.com>
2023-01-21 21:05:09 +0700

    less 2, task 9

---
+++ Less 2/Task 9.py
@@ -0,0 +1,5 @@
+string_one, string_two, string_tree = '5qwerty', '3python', '9calendar'
+
+print(string_one, int(string_one[0]) * string_one[1] + string_one[2:])
+print(string_two, int(string_two[0]) * string_two[1] + string_two[2:])
+print(string_tree, int(string_tree[0]) * string_tree[1] + string_tree[2:])
