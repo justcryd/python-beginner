a97251eac988469235d98658cb96fef16cc2567a
arlion <filatov_ty@sysbb.ru>
2023-01-23 14:47:16 +0700

    Task 2

--- Less 3/Task 1.py
+++ Less 3/Task 1.py
@@ -1,4 +1,15 @@
+# Вариант 1
+
 main_list = ['True', 'False', 'pop', 'remove', 'del']
 main_list.pop(-2)
 main_list.reverse()
+
 print(main_list)
+
+# Вариант 2
+
+main_list_v2 = ['True', 'False', 'pop', 'remove', 'del']
+del main_list_v2[-2]
+main_list_v2.reverse()
+
+print(main_list_v2)
