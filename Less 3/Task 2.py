76403ecf8bd0cfa0e85df74ce8004de9953a367e
arlion <filatov_ty@sysbb.ru>
2023-01-23 15:12:51 +0700

    Task 2

--- Less 3/Task 2.py
+++ Less 3/Task 2.py
@@ -1,4 +1,4 @@
-prog_languages = ['JavaScript', 'Java', 'Python', 'C#', 'PHP', 'C++',
-                  'Objective-C']
+prog_languages = ['JavaScript', 'Java', 'Python', 'C#', 'PHP', 'C++']
+prog_languages.append('Objective-C')
 
 print('\n'.join(prog_languages))
---
+++ Less 3/Task 3.py
@@ -0,0 +1,4 @@
+digits = [7, -2, '13', '1', '-5', 4]
+digits.insert()
+
+print(digits)
