b8088c3c804f0fe244d8a28fdf1ed5b48c210612
arlion <41083663+justcryd@users.noreply.github.com>
2023-01-26 23:59:56 +0700

    task 7

--- Less 3/Task 3.py
+++ Less 3/Task 3.py
@@ -1,4 +1,4 @@
 digits = [7, -2, '13', '1', '-5', 4]
-#digits.sort(key=int, reverse=True)
+digits.sort(key=int, reverse=True)
 
 print(digits)
--- Less 3/Task 6.py
+++ Less 3/Task 6.py
@@ -1,6 +1,11 @@
 digits = ['1','2','3','4','5','6','7','8','9','0']
 symbol = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
-digits.append(symbol)
 
-print(digits)
+
+two_list = digits + symbol
+letter_A = two_list.index('A')
+null_to_string = digits[-1] + digits[-1]
+# digits.append(symbol)
+
+print(null_to_string)
