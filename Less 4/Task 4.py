57c9a6309cb24be3af37e9bde40a09be9b0757fa
arlion <41083663+justcryd@users.noreply.github.com>
2023-01-29 22:50:11 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Less 3/Task 6.py
    #	Less 3/Task 7.py

--- Less 4/Task 4.py
+++ Less 4/Task 4.py
@@ -2,4 +2,6 @@ creds = ('__init__', '127.0.0.1', 'admin', 'SupermAn', 'ClOwN',
          'http://site.com', 'humorist', 'https://example.com',
          '192.168.12.101', 12345, 'https://yandex.ru')
 
+
+
 print(creds)
