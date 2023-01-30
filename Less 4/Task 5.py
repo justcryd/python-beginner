90c60ca243d5b608f2d2c4018842eb44e9e4092f
justcryd <adm.nkvz@gmail.com>
2023-01-30 15:09:46 +0700

    less 2, task 9

--- Less 4/Task 5.py
+++ Less 4/Task 5.py
@@ -2,7 +2,9 @@ access = {'admin': 'megapass', 'mazhor': 'goldkey777', 'chuvak': 'all_good',
           'lelik': 'bablo_forever', 'invisible_man': 'a1dfvb5415vD',
           'LOL': 12345}
 
-login = access['admin']
-
-print(login)
-
+print('admin:', access.get('admin'))
+print('mazhor:', access.get('mazhor'))
+print('chuvak:', access.get('chuvak'))
+print('lelik:', access.get('lelik'))
+print('invisible_man:', access.get('invisible_man'))
+print('LOL:', access.get('LOL'))
