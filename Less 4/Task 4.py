e18d935f6713a00e68e99deb7d0f1ef90a841d31
justcryd <adm.nkvz@gmail.com>
2023-01-30 14:41:16 +0700

    less 2, task 9

--- Less 4/Task 4.py
+++ Less 4/Task 4.py
@@ -2,6 +2,12 @@ creds = ('__init__', '127.0.0.1', 'admin', 'SupermAn', 'ClOwN',
          'http://site.com', 'humorist', 'https://example.com',
          '192.168.12.101', 12345, 'https://yandex.ru')
 
+logins = ('Logins:', creds[2], creds[6])
+passwords = ('Passwords:', creds[3], creds[4])
+ips = ('IP:', creds[1], creds[-3])
+hosts = ('Hosts:', creds[5], creds[7])
 
-
-print(creds)
+print('\n'.join(logins), '\n')
+print('\n'.join(passwords), '\n')
+print('\n'.join(ips), '\n')
+print('\n'.join(hosts))
--- Less 4/Task 5.py
+++ Less 4/Task 5.py
@@ -2,4 +2,7 @@ access = {'admin': 'megapass', 'mazhor': 'goldkey777', 'chuvak': 'all_good',
           'lelik': 'bablo_forever', 'invisible_man': 'a1dfvb5415vD',
           'LOL': 12345}
 
-print(access['admin'])
+login = access['admin']
+
+print(login)
+
