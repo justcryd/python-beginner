e3f5b22784ff99286c143fcea89d75fd1c0253b4
justcryd <adm.nkvz@gmail.com>
2023-01-30 17:06:05 +0700

    less 2, task 9

---
+++ Less 4/Task 6.py
@@ -0,0 +1,7 @@
+target_dict = {'o': 3, 'd': 7, 'k': 5, 'w': 2, 'e': 6, 'g': 1, 'y': 4}
+
+summa = target_dict.values()
+
+print('okey')
+print('Сумма значений словаря:', sum(target_dict.values()))
+print('Отсортированный список значений словаря:', type(summa))
