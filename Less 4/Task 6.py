3739504c3f2005f8b2ca35c788f03ba7dea21b6d
justcryd <adm.nkvz@gmail.com>
2023-01-31 13:31:26 +0700

    task 6

--- Less 4/Task 6.py
+++ Less 4/Task 6.py
@@ -1,7 +1,8 @@
 target_dict = {'o': 3, 'd': 7, 'k': 5, 'w': 2, 'e': 6, 'g': 1, 'y': 4}
 
-summa = target_dict.values()
-
 print('okey')
 print('Сумма значений словаря:', sum(target_dict.values()))
-print('Отсортированный список значений словаря:', type(summa))
+print('Отсортированный список значений словаря:',
+      list(sorted(target_dict.values())))
+print('Сумма третьих значений словоря:',
+      sum(list(target_dict.values())[::3]))
