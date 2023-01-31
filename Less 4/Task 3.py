1cd6537585a453d7a8ad9d4ac76a98cc566812c8
arlion <41083663+justcryd@users.noreply.github.com>
2023-01-31 21:07:04 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Less 3/Task 6.py
    #	Less 3/Task 7.py

--- Less 4/Task 3.py
+++ Less 4/Task 3.py
@@ -1,4 +1,4 @@
 new_set = {'sqlmap', 'IronWASP', 'Burp Suite', 'Nikto', 'ATSCAN', 'BruteXSS',
            'WFUZZ'}
 
-print(str(sorted(new_set, reverse=True)).replace('[', '').replace(']', ''))
+print(str(sorted(new_set, reverse=True))[1:-1])
--- Less 4/Task 5.py
+++ Less 4/Task 5.py
@@ -2,9 +2,9 @@ access = {'admin': 'megapass', 'mazhor': 'goldkey777', 'chuvak': 'all_good',
           'lelik': 'bablo_forever', 'invisible_man': 'a1dfvb5415vD',
           'LOL': 12345}
 
-print('admin:', access.get('admin'))
-print('mazhor:', access.get('mazhor'))
-print('chuvak:', access.get('chuvak'))
-print('lelik:', access.get('lelik'))
-print('invisible_man:', access.get('invisible_man'))
-print('LOL:', access.get('LOL'))
+print(str(access)[2:20].replace("'", ''))
+print(str(access)[22:43].replace("'", ''))
+print(str(access)[46:66].replace("'", ''))
+print(str(access)[68:91].replace("'", ''))
+print(str(access)[94:125].replace("'", ''))
+print(str(access)[128:139].replace("'", ''))
