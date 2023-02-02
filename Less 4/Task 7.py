c9f09eb8c1c85990517dceea61e34708e7034f73
arlion <41083663+justcryd@users.noreply.github.com>
2023-02-02 21:02:21 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Less 3/Task 6.py
    #	Less 3/Task 7.py

--- Less 4/Task 7.py
+++ Less 4/Task 7.py
@@ -2,15 +2,8 @@ my_dict = {1: ("прокоп", "порок"), 2: ("сушилка", "осушк�
            3: ("вязанка", "навязка"), 4: ("каторга", "рогатка"),
            5: ("плесень", "полдник")}
 
-one_word_one, one_word_two = my_dict[1]
-two_word_one, two_word_two = my_dict[2]
-tree_word_one, tree_word_two = my_dict[3]
-four_word_one, four_word_two = my_dict[4]
-five_word_one, five_word_two = my_dict[5]
-
-print(' '.join(one_word_one) >= ' '.join(one_word_two))
-print(' '.join(two_word_one) >= ' '.join(two_word_two))
-print(' '.join(tree_word_one) >= ' '.join(tree_word_two))
-print(' '.join(four_word_one) >= ' '.join(four_word_two))
-print(' '.join(five_word_one) >= ' '.join(five_word_two))
-#print(' '.join(one_word_one))
+print(*my_dict[1], my_dict[1][0][::1] >= my_dict[1][1][::1])
+print(*my_dict[2], my_dict[2][0][::1] >= my_dict[2][1][::1])
+print(*my_dict[3], my_dict[3][0][::1] >= my_dict[3][1][::1])
+print(*my_dict[4], my_dict[4][0][::1] >= my_dict[4][1][::1])
+print(*my_dict[5], my_dict[5][0][::1] >= my_dict[5][1][::1])
