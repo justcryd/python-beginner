8fc18e7c86e4e78e68a8d6a6d159a0d3a918e94c
arlion <41083663+justcryd@users.noreply.github.com>
2023-02-02 21:28:29 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Less 3/Task 6.py
    #	Less 3/Task 7.py

--- Less 4/Task 5.py
+++ Less 4/Task 5.py
@@ -2,9 +2,4 @@ access = {'admin': 'megapass', 'mazhor': 'goldkey777', 'chuvak': 'all_good',
           'lelik': 'bablo_forever', 'invisible_man': 'a1dfvb5415vD',
           'LOL': 12345}
 
-print(str(access)[2:20].replace("'", ''))
-print(str(access)[22:43].replace("'", ''))
-print(str(access)[46:66].replace("'", ''))
-print(str(access)[68:91].replace("'", ''))
-print(str(access)[94:125].replace("'", ''))
-print(str(access)[128:139].replace("'", ''))
+print(str(access)[1:-1].replace(", ", "\n").replace("'", ''))
