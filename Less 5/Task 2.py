b521e59294c36986feb80f9b7a8bef711ce91850
arlion <41083663+justcryd@users.noreply.github.com>
2023-02-12 19:52:38 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Less 3/Task 6.py
    #	Less 3/Task 7.py

--- Less 5/Task 2.py
+++ Less 5/Task 2.py
@@ -1,12 +1,12 @@
 user_number = int(input('Введите возраст: '))
 
-if 1 <= user_number <= 6:
+if user_number == 0 or user_number > 100:
+    print('Введите число от 1 до 100')
+elif 1 <= user_number <= 6:
     print('Детство это - прекрасно!')
-elif 7 <= user_number <= -17:
+elif 7 <= user_number <= 17:
     print('Учиться, учиться, учиться...')
 elif 18 <= user_number <= 64:
     print('Теперь ты можешь делать всё что угодно!')
 elif 65 <= user_number <= 100:
     print('Заслуженный отдых')
-else:
-    print('Введите число от 1 до 100')
